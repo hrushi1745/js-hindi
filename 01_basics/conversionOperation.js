348ca714117036d712da6ff10e5544e383ea4d2f
hrushi1745 <ajgaonkarhrushikesh090@gmail.com>
2025-11-19 10:13:11 +0000

    conversion of data types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,28 @@
+//let score="9abc"
+
+//console.log(typeof score)
+
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//Notes
+//"33" =>33
+//"33anc" =>NaN
+//true =>1 , false=>0
+
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+//1=>true or 0 =>false
+//"Hitesh " =>true
+
+
+let someNumner=33
+
+let stringNumber=String(someNumner)
+console.log(stringNumber)
+console.log(typeof stringNumber)
---
+++ 01_basics/data_types.js
@@ -0,0 +1,16 @@
+"user strict"; //treat all js code as new version
+
+//alert(3+3) //we are using node js  not brower
+
+console.log(3+3); // code rediability should be high
+
+//number 2 to power 53
+//bigint
+//string => ""
+//boolean =true & flase
+//null =>standaalone value
+//undefined =>
+//symbol =>unique
+
+console.log(typeof "hrushi");
+console.log(typeof 20);
