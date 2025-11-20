8e669efe6914ad1f7a9aff6aff5546883a38683a
hrushi1745 <ajgaonkarhrushikesh090@gmail.com>
2025-11-20 09:31:01 +0000

    summary

---
+++ 01_basics/summary.js
@@ -0,0 +1,24 @@
+// const score=100;
+// console.log(score);
+ 
+// const scoreValue=200.1
+// const IsLogin=true;
+const outsideTemp=null;
+// console.log([scoreValue, IsLogin, outsideTemp]);
+
+const id=Symbol('1234')
+const anotherId=Symbol('4567')
+
+// console.log(id===anotherId)
+
+//Array , object , Function
+
+// const heros=["Hrushi","Shlok","Gaurang","Arshad"];
+
+// let myobg={name:"Hrushi",age:10,Gender:"Male", }
+const myFunction=function(){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherId)
+
