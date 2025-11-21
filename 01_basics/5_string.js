a953287ecc0ce1f38fe10233678717d0c13da38e
hrushi1745 <ajgaonkarhrushikesh090@gmail.com>
2025-11-21 09:11:54 +0000

    string in js

---
+++ 01_basics/5_string.js
@@ -0,0 +1,37 @@
+const name="Hrushi";
+const repocount=20
+
+// console.log(`My name is ${name} and my repocount is ${repocount}`)
+
+
+const gamename=new String("hrushi@1234")
+
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.toLocaleUpperCase());
+// console.log(gamename.toLowerCase());
+// console.log(gamename.charAt(1))
+
+//slicing 
+
+const newGnamename=gamename.substring(0,5)
+// console.log(newGnamename)
+
+const anothername=gamename.slice(-11,1)
+// console.log(anothername);
+
+//Trim
+const names="   Hrushi   ";
+// console.log(names);
+// console.log(names.trim())
+
+//Replace 
+const url="https//hrushi-20-ajgonakar";
+// console.log(url.replace('%20','-'))
+// console.log(url.includes('hrushi'))
+// console.log(url.includes('jayraj'))
+// console.log(url.split('-'));
+
+
