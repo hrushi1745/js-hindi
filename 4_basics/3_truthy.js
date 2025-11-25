1c06ad7105abca48431d3152ff7f230ae45c43b5
hrushi1745 <ajgaonkarhrushikesh090@gmail.com>
2025-11-25 11:01:22 +0000

    truthy

---
+++ 4_basics/3_truthy.js
@@ -0,0 +1,38 @@
+const useremail="hrushi@123"
+
+// if(useremail){
+//     console.log("Got a useremail...");
+// }else{
+//     console.log("Not got a useremaili...");
+    
+// }
+
+// if(useremail.length===0){
+//     console.log("Array is empty");
+    
+// }
+
+// const emptyobject={};
+
+// if(Object.keys(emptyobject).length===0){
+//     console.log("Object is empty...");
+    
+// }
+
+//Nullish coliscing operator(??)//null undefined
+
+let val1;
+// val1=5 ?? 110
+// val1=null ?? 110
+val1=undefined?? 110
+val1=null??5 ?? 110
+// console.log(val1);
+
+//Ternary Operator
+
+//condition ?true : false
+
+const iceprice=100
+iceprice <=80 ?console.log("Less than 80"):console.log("More than 80");
+
+
