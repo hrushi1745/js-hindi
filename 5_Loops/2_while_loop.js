ad88ee4f6cc45766afacd81dd789a9096051c540
hrushi1745 <ajgaonkarhrushikesh090@gmail.com>
2025-11-25 11:57:04 +0000

    while and do-while loop

---
+++ 5_Loops/2_while_loop.js
@@ -0,0 +1,35 @@
+let index=0
+
+while(index <=20){
+    // console.log(`value is ${index}`);
+    index=index+2
+    
+}
+
+let myarray=['hrushi','jayraj','Gaurang']
+
+let arr=0
+
+while(arr<myarray.length){
+    // console.log(`value is ${myarray[arr]}`);
+    arr +=1;
+    
+}
+
+//do while loop
+
+// let score=1;
+
+// do{
+//     console.log(`score is ${score}`);
+//     score++
+    
+// }while(score<=10);
+
+let score=11;
+
+do{
+    console.log(`score is ${score}`);
+    score++
+    
+}while(score<=10);
