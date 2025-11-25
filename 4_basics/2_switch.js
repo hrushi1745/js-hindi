ab232dd98102b2f88e7244549cbd5da74f8bd2f3
hrushi1745 <ajgaonkarhrushikesh090@gmail.com>
2025-11-25 10:35:47 +0000

    switch_statement

---
+++ 4_basics/2_switch.js
@@ -0,0 +1,18 @@
+//Switch Statement
+
+const month="feb";
+
+switch(month){
+    case "jan":
+        console.log("Janauary");
+        break;
+    case "feb":
+        console.log("Febuaty");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    default:
+        console.log("Invalid choise");
+        
+}
