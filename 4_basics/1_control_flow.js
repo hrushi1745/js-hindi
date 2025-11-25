358a51366c77e8e73a3284729dfb27916ab367cf
hrushi1745 <ajgaonkarhrushikesh090@gmail.com>
2025-11-25 10:27:15 +0000

    control_statement

---
+++ 4_basics/1_control_flow.js
@@ -0,0 +1,63 @@
+// //if
+// // >,<,>=,<=,!=,==,===
+
+// const userloggedin=true;
+
+// if(2==="2"){
+//     // console.log("Executed...");
+
+// }else{
+//     // console.log("False...")
+// }
+
+// // const temp=51
+
+// // if(temp===50){
+// //     console.log("Tempretre is less than 50");
+// // }else{
+// //     console.log("Temp is greater than 50")
+// // }
+// // console.log("execute");
+
+// const score=500
+
+// if(score>200){
+//     const power="fly"
+//     // console.log(`Power is ${power}`);
+    
+    
+// //short hand notation
+// // const bal=1000;
+// // if (bal > 500) console.log("test"), console.log("test2")
+
+const bal=1000
+
+// if(bal<800){
+//     console.log("Less than 800");
+    
+// }else if(bal<700){
+//     console.log("Less than 700");
+// }else if(bal<500){
+//     console.log("Less than 500");
+// }else{
+//     console.log("Less than 1100");
+// }
+
+//AND operaion
+const userloggedin=true;
+const debitcard=true;
+
+if(userloggedin && debitcard){
+    // console.log("Allow to uset buy a course...");
+    
+}
+
+//OR operation
+const loginFromegoogle=true;
+const userloggedinn=false;
+
+if(loginFromegoogle || userloggedinn){
+    console.log("Allow to login...")
+}else{
+    console.log("Unable to Login...");
+}
